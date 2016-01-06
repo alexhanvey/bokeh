d522f34887fe31c1acf6b137f5de6ee84befaba8
Sarah Bird <sarah@bonvaya.com>
2016-01-06 15:42:21 -0500

    Try fixing flexx example

--- sphinx/source/docs/user_guide/source_examples/interaction_callbacks_in_python.py
+++ sphinx/source/docs/user_guide/source_examples/interaction_callbacks_in_python.py
@@ -1,10 +1,11 @@
-from bokeh.io import vform
+
+from bokeh.io import vplot
 from bokeh.models import CustomJS, ColumnDataSource, Slider
 from bokeh.plotting import Figure, output_file, show
 
-output_file("callback.html")
+output_file("callbacks_in_python.html")
 
-x = [x*0.005 for x in range(0, 200)]
+x = [x * 0.005 for x in range(0, 200)]
 y = x
 
 source = ColumnDataSource(data=dict(x=x, y=y))
@@ -12,17 +13,20 @@ source = ColumnDataSource(data=dict(x=x, y=y))
 plot = Figure(plot_width=400, plot_height=400)
 plot.line('x', 'y', source=source, line_width=3, line_alpha=0.6)
 
+
 def callback(source=source):
     data = source.get('data')
-    f = cb_obj.get('value')
+    f = slider.get('value')
     x, y = data['x'], data['y']
     for i in range(len(x)):
         y[i] = Math.pow(x[i], f)
-    source.trigger('change');
+    source.trigger('change')
 
-slider = Slider(start=0.1, end=4, value=1, step=.1, title="power", 
-                callback=CustomJS.from_py_func(callback))
+callback = CustomJS.from_py_func(callback)
 
-layout = vform(slider, plot)
+slider = Slider(start=0.1, end=4, value=1, step=.1, title="power", callback=callback)
+callback.args['slider'] = slider
+
+layout = vplot(slider, plot)
 
 show(layout)
